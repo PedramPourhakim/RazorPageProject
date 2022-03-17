b21131cf03e11d07caf7cfbeff47afb5c332b923
PedramPourhakim <shemuel1226@gmail.com>
2022-03-17 14:23:55 +0330

    Create Article Html Page & Model Implemented

---
+++ MyRazorProject/Models/CreateArticle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRazorProject.Models
+{
+    public class CreateArticle
+    {
+        public string Title { get; set; }
+        public string Picture { get; set; }
+        public string PictureAlt { get; set; }
+        public string PictureTitle { get; set; }
+        public string ShortDescription { get; set; }
+        public string Body { get; set; }
+    }
+}
--- MyRazorProject/Pages/CreateArticle.cshtml.cs
+++ MyRazorProject/Pages/CreateArticle.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyRazorProject.Models;
 
 namespace MyRazorProject.Pages
 {
     public class CreateArticleModel : PageModel
     {
+        private readonly BlogContext _context;
+        public CreateArticleModel(BlogContext _context)
+        {
+            this._context = _context;
+        }
         public void OnGet()
         {
+        }
+        public void OnPost(CreateArticle command)
+        {
+            var article =
+                new Article(command.Title, command.Picture, command.PictureAlt, command.PictureTitle, command.ShortDescription, command.Body);
         }
     }
 }
